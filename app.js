82a80e9c2951df1dae6ccb482eb9ac4f5849d5ad
Mazukiri <bestluxvn2004@gmail.com>
2025-04-19 20:09:20 +0700

    Viết Hello World trong NodeJS

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.get("/hoangduc", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port: ${PORT}`)
+})
