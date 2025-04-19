8218ee929b2529889af9e811f2964c1074027b5c
Mazukiri <bestluxvn2004@gmail.com>
2025-04-19 20:22:34 +0700

    Setup TypeScript cho dự án NodeJS

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+import express from "express"
+
 app.get("/hoangduc", (req, res) => {
     res.send("Hello World")
 })
---
+++ dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.get("/hoangduc", (req, res) => {
+    res.send("Hello World");
+});
+app.listen(PORT, () => {
+    console.log(`My app is running on port: ${PORT}`);
+});
+//# sourceMappingURL=app.js.map
